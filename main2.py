b5ad1a01ec83cbd115488d10a4d1229ce321ff6b
CorentinCARTALL <corentin.Cartallier@gmail.com>
2025-12-03 14:12:38 +0100

    Version finales fonctionnelles

--- main2.py
+++ main2.py
@@ -8,14 +8,26 @@ code =0
 for i in range(len(instructions)):
     sens = instructions[i][0]
     valeur =int(instructions[i][1:])
-    print("Instruction : ",instructions[i])
-
-    print(valeur)
+    print("--------------Instruction : ",instructions[i])
+    print("Position initiale : ", position)
+    print("Code avant: ",code)
     if sens =="L":
-        position += valeur
+        distance_zero = position
+        if position ==0:
+            distance_zero =100
+        if valeur >= distance_zero:
+            code +=1
+            reste= valeur-distance_zero
+            code += reste//100
+        position =(position-valeur)%100
     if sens == "R":
-        position -= valeur
+        distance_zero = 100-position
+        if valeur >= distance_zero:
+            code +=1
+            reste = valeur- distance_zero
+            code += reste//100
+        position =(position+valeur)%100
     print("Position après instruction",position)
-    print("Code: ",code)
+    print("Code après : ",code)
 
 print(code)
